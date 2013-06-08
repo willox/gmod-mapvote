ca68c4d3c9b4e40e99206b75816c7d97e6faf3b3
Tyrantelf <tyrantelf@iongaming.org>
2013-06-08 11:19:01 -0500

    Fixes error being thrown from old fretta function

--- lua/autorun/server/sv_tttvote.lua
+++ lua/autorun/server/sv_tttvote.lua
@@ -1,4 +1,4 @@
-hook.Add( "Initialize", "RTV", function()
+hook.Add( "Initialize", "AutoTTTMapVote", function()
         function CheckForMapSwitch()
            -- Check for mapswitch
            local rounds_left = math.max(0, GetGlobalInt("ttt_rounds_left", 6) - 1)
@@ -15,15 +15,9 @@ hook.Add( "Initialize", "RTV", function()
               LANG.Msg("limit_time", {mapname = nextmap})
               switchmap = true
             end
-            if GAMEMODE_NAME == "terrortown" then
-              if switchmap then
+            if GAMEMODE_NAME == "terrortown" and switchmap then
                 timer.Stop("end2prep")
                 MapVote.Start(nil, nil, nil, nil)
-              elseif ShouldMapSwitch() then
-                    LANG.Msg("limit_left", {num = rounds_left,
-                              time = math.ceil(time_left / 60),
-                              mapname = "chosen by a vote"})
-              end
             end
         end
 end )
